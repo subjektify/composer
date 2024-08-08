995a39b5bf693ff19655325f1325232453720c22
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2024-08-08 05:39:08 -0700

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,6 +2,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+//TODO: Switch to nodl https://www.usenodl.dev/docs/packages/react
+
 const config: Config = {
   title: 'Composer',
   tagline: 'No-code subject creation and management',
